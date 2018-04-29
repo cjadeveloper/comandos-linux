57ef5cf053689be4b879511e4bc7852225f7c6ed
Cristian J. Azulay <cjadesarrollador@gmail.com>
2018-04-29 04:41:57 -0300

    Create settings.py

---
+++ Django/settings.py
@@ -0,0 +1,8 @@
+# Cambio de configuraciones por default
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.0/topics/i18n/
+
+LANGUAGE_CODE = 'es-AR'
+
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
