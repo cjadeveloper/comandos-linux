87b4522afab5ce6efb2dfb69b42ffbd4c6cc4251
Cristian J. Azulay <cjadesarrollador@gmail.com>
2018-08-21 20:37:05 -0300

    Update settings.py

--- Django/settings.py
+++ Django/settings.py
@@ -1,5 +1,25 @@
 # Cambio de configuraciones por default
 
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_name',
+        'USER': 'db_user',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    },
+    
+    'extra': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
